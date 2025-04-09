ecda145cdd7eaed5608e05be20e2c14b2f86d4db
arthur.gino <arthur.gino@RESERVA>
2025-04-09 10:09:13 -0300

    Add project files.

---
+++ TaskAPI/Context/TarefasContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskAPI.Model;
+namespace TaskAPI.Context
+{
+    public class TarefasContext : DbContext
+    {
+        public TarefasContext(DbContextOptions<TarefasContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarefas> Tarefas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         modelBuilder.Entity<Tarefas>().ToTable("Tarefa");
+
+
+        }
+    }
+}
---
+++ TaskAPI/Model/Tarefas.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskAPI.Model
+{
+    public class Tarefas
+    {
+        public int Id { get; set; }
+        public string Tarefa { get; set; }
+        public bool Status { get; set; }
+        public DateTime DataTarefa { get; set; }
+    }
+}
