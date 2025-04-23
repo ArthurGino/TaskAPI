b1f595afc51d479110d89cf8e3d6785754ee1440
arthur.gino <arthur.gino@RESERVA>
2025-04-23 16:57:16 -0300

    token

---
+++ TaskAPI/Authorize.cs
@@ -0,0 +1,39 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using TaskAPI.Model;
+
+namespace TaskAPI
+{
+    public class Authorize
+    {
+        public string CreateToken(Register usuario)
+        {
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim("Id", usuario.Id.ToString()),
+                new Claim("Email", usuario.Email.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("AppSettings:DefaultConnection"));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+               claims: claims,
+               expires: DateTime.Now.AddDays(1),
+               signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+
+
+
+        }
+    }
+
+
+}
